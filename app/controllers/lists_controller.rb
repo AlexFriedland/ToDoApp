c3eda35733cbf713f1d33c3ee9fcd8f043349d90
AlexFriedland <friedlandaj@gmail.com>
2018-05-25 16:10:11 +0200

    end 1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,22 @@
 class ListsController < ApplicationController
   def index
     #implicitly rendering 'lists#index.html.erb'
+    @list = List.new
     @lists = List.all
   end
 
   def show
     @list = List.find_by(params[:id])
+  end
+
+  def create
+    #read activerecord strong_parameters
+    @list = List.create!(name: params[:list][:name])
+    redirect_to list_path(@list)
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
