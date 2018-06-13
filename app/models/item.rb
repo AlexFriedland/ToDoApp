99dd6ad20474e546160186157004dc928b80f24e
AlexFriedland <friedlandaj@gmail.com>
2018-06-13 13:03:33 +0200

    load JS

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,18 @@ class Item < ApplicationRecord
   belongs_to :list
 
   validates :description, presence: true
+
+  #DICTIONARY
+  STATUS = {
+    incomplete: 0,
+    complete: 1
+  }
+
+  def complete?
+    self.status == STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STATUS[:incomplete]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_06_12_113520) do
   create_table "items", force: :cascade do |t|
     t.string "description"
     t.integer "list_id"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
