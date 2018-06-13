8fa7aa62bf2846cb3110594b1af12530439e46bd
AlexFriedland <friedlandaj@gmail.com>
2018-06-13 12:03:59 +0200

    get html + js for checkbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
 end
+
+
+=begin
+
+<input class="toggle" type="checkbox">
+
+$(function(){
+  $("input.toggle").on("change", function(){
+    $(this).parents("form")trigger("submit")
+  })
+});
+
+=end
